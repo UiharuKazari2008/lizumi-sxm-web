3b004c5d714883c689c9387bb516483c428357e3
Yukimi Kazari <dev@9854.moe>
2022-06-19 13:15:42 -0400

    Added Type 1 Sequenzia REAL application interface

--- public/js/pre-scripts.js
+++ public/js/pre-scripts.js
@@ -3,9 +3,26 @@ const key = (typeof SEQ_APP_URL !== 'undefined') ? false : (searchParams.has('ke
 let refreshTimer = null;
 let notifyCenter
 
+function msToTime(s) {
+    // Pad to 2 or 3 digits, default is 2
+    function pad(n, z) {
+        z = z || 2;
+        return ('00' + n).slice(-z);
+    }
+
+    var ms = s % 1000;
+    s = (s - ms) / 1000;
+    var secs = s % 60;
+    s = (s - secs) / 60;
+    var mins = s % 60;
+    var hrs = (s - mins) / 60;
+
+    return [pad(hrs), pad(mins), pad(secs)];
+}
+
 function startStatusUpdater() {
     clearInterval(refreshTimer);
-    refreshTimer = setInterval(getTunersStatus, 15000);
+    refreshTimer = setInterval(getTunersStatus, 60000);
     return false;
 }
 
